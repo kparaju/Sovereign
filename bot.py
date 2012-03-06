c95117fce5daa19d5563484e4953265c1aeae697
Kshitij Parajuli <kshitijparajuli@gmail.com>
2012-03-06 00:48:05 -0500

    Bot can connect to server

--- bot.py
+++ bot.py
@@ -1,27 +1,32 @@
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
-
-import time, sys
-
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from orders import *
 
 class IAMABot(irc.IRCClient):
-	
-	sovereign = Sovereign("Sovereign_Beta")
-
-	engine = create_engine('sqlite:///:memory:', echo=True)
-
-	def connectionMade(self):
-		irc.IRCClient.connectionMade(self)
+	def __init__(self):
+		engine = create_engine('sqlite:///bot.db')
+		Session = sessionmaker(bind=engine)
 		
+		self.session = Session()
+		self.sovereign = self.session.query(Sovereign).first()
+		self.nickname = self.sovereign.nickname.encode()
 
-	def connectionLost(self, reason):
-		irc.IRCClient.connectionLost(self, reason)
-		
+	def signedOn(self):
+		for chan in self.sovereign.ircchannels:
+			self.join(chan.name.encode(), chan.key.encode())
+
 	def privmsg(self, user, channel, msg):
 		nick = user.split('!',1)[0]
 
-		if (msg == "@hello"):
+class SovereignFactory(protocol.ClientFactory):
+	def buildProtocol(self, addr):
+		bot = IAMABot()
+		bot.factory = self
+		return bot
 
-
-	
+if __name__ == '__main__':
+	sovereign = SovereignFactory()
+	reactor.connectTCP("irc.rizon.net", 6667, ssovereign)
+	reactor.run()
