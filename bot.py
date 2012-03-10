ebbf4abd3af54918e0fba9318cb1cedfa7735561
Kshitij Parajuli <kshitijparajuli@gmail.com>
2012-03-09 19:43:32 -0500

    Mostly code reformatting, plus some new code to read orders

--- bot.py
+++ bot.py
@@ -5,28 +5,43 @@ from sqlalchemy.orm import sessionmaker
 from orders import *
 
 class IAMABot(irc.IRCClient):
-	def __init__(self):
-		engine = create_engine('sqlite:///bot.db')
-		Session = sessionmaker(bind=engine)
-		
-		self.session = Session()
-		self.sovereign = self.session.query(Sovereign).first()
-		self.nickname = self.sovereign.nickname.encode()
+    def __init__(self):
+        engine = create_engine('sqlite:///bot.db')
+        Session = sessionmaker(bind=engine)
 
-	def signedOn(self):
-		for chan in self.sovereign.ircchannels:
-			self.join(chan.name.encode(), chan.key.encode())
+        self.session = Session()
+        # Just use the first database entry right now
+        self.sovereign = self.session.query(Sovereign).first()
+        self.nickname = self.sovereign.nickname.encode()
 
-	def privmsg(self, user, channel, msg):
-		nick = user.split('!',1)[0]
+    def signedOn(self):
+        for chan in self.sovereign.ircchannels:
+            self.join(chan.name.encode(), chan.key.encode())
+
+    def privmsg(self, user, channel, msg):
+        order_commands = {}
+        for orderset in self.sovereign.ordersets:
+            order_commands["@" + orderset.name] = orderset
+            order_commands["@update" + orderset.name] = orderset
+
+        nick = user.split('!', 1)[0]
+        msgsplit = msg.split(' ')
+        if (msgsplit[0] in order_commands):
+            if (msgsplit[0].find('@update') != 0):
+                showOrder(order_commands[msgsplit[0]], user, channel, msg)
+
+    def showOrder(self, orderset, user, channel, msg):
+        for order in orderset:
+            self.msg(channel, order.encode())
+
 
 class SovereignFactory(protocol.ClientFactory):
-	def buildProtocol(self, addr):
-		bot = IAMABot()
-		bot.factory = self
-		return bot
+    def buildProtocol(self, addr):
+        bot = IAMABot()
+        bot.factory = self
+        return bot
 
 if __name__ == '__main__':
-	sovereign = SovereignFactory()
-	reactor.connectTCP("irc.rizon.net", 6667, ssovereign)
-	reactor.run()
+    sovereign = SovereignFactory()
+    reactor.connectTCP("irc.rizon.net", 6667, sovereign)
+    reactor.run()
--- orders.py
+++ orders.py
@@ -6,75 +6,78 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Sovereign(Base):
-	__tablename__ = 'sovereign'
-	
-	id = Column(Integer, Sequence('sovereign_id_seq'), primary_key=True)
-	
-	nickname = Column(String)
-	
-	ordersets = relationship('OrderSet', order_by = 'OrderSet.id', backref='sovereign')
-	ircchannels = relationship('IRCChannel', order_by = 'IRCChannel.name', backref='sovereign')
-	ircusers = relationship('IRCUser', order_by = 'IRCUser.nick', backref='sovereign')
-	
-	def __init__(self, nickname):
-		self.nickname = nickname
-	
+    __tablename__ = 'sovereign'
+
+    id = Column(Integer, Sequence('sovereign_id_seq'), primary_key=True)
+
+    nickname = Column(String)
+
+    ordersets = relationship('OrderSet', order_by='OrderSet.id', backref='sovereign')
+    ircchannels = relationship('IRCChannel', order_by='IRCChannel.name', backref='sovereign')
+    ircusers = relationship('IRCUser', order_by='IRCUser.nick', backref='sovereign')
+
+    def __init__(self, nickname):
+        self.nickname = nickname
+
+
 class IRCUser(Base):
-	__tablename__ = 'user'
-	nick = Column(String, primary_key=True)
-	admin = Column(Boolean)
-	sovereign_id = Column(Integer, ForeignKey('sovereign.id'))
+    __tablename__ = 'user'
+    nick = Column(String, primary_key=True)
+    admin = Column(Boolean)
+    sovereign_id = Column(Integer, ForeignKey('sovereign.id'))
 
-	def __init__(self, nick):
-		self.nick = nick
+    def __init__(self, nick):
+        self.nick = nick
 
-	def __repr__(self):
-		return "<IRCUser ('%s','%s')>" % (self.nick, self.admin)
+    def __repr__(self):
+        return "<IRCUser ('%s','%s')>" % (self.nick, self.admin)
+
 
 class IRCChannel(Base):
-	__tablename__ = 'ircchannels'
-	name = Column(String, primary_key=True)
-	key = Column(String)
-	sovereign_id = Column(Integer, ForeignKey('sovereign.id'))
-	
-	def __init__(self, name, key = ''):
-		self.name = name
-		self.key = key
-	
-	def __repr__(self):
-		return "<IRCChannel ('%s', '%s')>" % (self.name, self.key)
-	
+    __tablename__ = 'ircchannels'
+    name = Column(String, primary_key=True)
+    key = Column(String)
+    sovereign_id = Column(Integer, ForeignKey('sovereign.id'))
+
+    def __init__(self, name, key=''):
+        self.name = name
+        self.key = key
+
+    def __repr__(self):
+        return "<IRCChannel ('%s', '%s')>" % (self.name, self.key)
+
+
 class OrderSet(Base):
-	__tablename__ = 'ordersets'
+    __tablename__ = 'ordersets'
 
-	id = Column(Integer, Sequence('orderset_id_seq'), primary_key=True)
-	name = Column(String)
-	orders = relationship('Order', order_by = 'Order.id', backref='orderset')
-	sovereign_id = Column(Integer, ForeignKey('sovereign.id'))
+    id = Column(Integer, Sequence('orderset_id_seq'), primary_key=True)
+    name = Column(String)
+    orders = relationship('Order', order_by='Order.id', backref='orderset')
+    sovereign_id = Column(Integer, ForeignKey('sovereign.id'))
 
-	def __init__(self, name):
-		self.name = name
-	
-	def __repr__(self):
-		return "<OrderSet %s: %s>" % (self.name, self.orders)
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<OrderSet %s: %s>" % (self.name, self.orders)
 
 
 class Order(Base):
-	__tablename__ = 'orders'
+    __tablename__ = 'orders'
 
-	id = Column(Integer, Sequence('order_id_seq'), primary_key=True)
-	territory = Column(String)
-	url = Column(String)
-	info = Column(String)
+    id = Column(Integer, Sequence('order_id_seq'), primary_key=True)
+    territory = Column(String)
+    url = Column(String)
+    info = Column(String)
 
-	orderset_id = Column(Integer, ForeignKey('ordersets.id'))
+    orderset_id = Column(Integer, ForeignKey('ordersets.id'))
 
-	def __init__(self, territory, url, info):
-		self.territory = territory
-		self.url = url
-		self.info = info
+    def __init__(self, territory, url, info):
+        self.territory = territory
+        self.url = url
+        self.info = info
 
-	def __repr__(self):
-		return "<Order('%s','%s','%s')>" % (self.territory, self.url, self.info)
+    def __repr__(self):
+        return "<Order('%s','%s','%s')>" % (self.territory, self.url, self.info)
 
 
