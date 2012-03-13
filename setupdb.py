6052037f8c52f661a860bba444d86115152aa549
Kshitij Parajuli <kshitijparajuli@gmail.com>
2012-03-12 20:57:39 -0400

    Sets up a sample database for first time use

---
+++ setupdb.py
@@ -0,0 +1,42 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from orders import *
+from sqlalchemy.orm import sessionmaker
+import os.path
+
+engine = create_engine('sqlite:///sovereign.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+if (os.path.isfile("sovereign.db")):
+    print "database already exists"
+    exit()
+
+Base.metadata.create_all(engine)
+
+# Todo create the schema (look it up)
+
+s = Sovereign("Sovereign-Beta")
+c = IRCChannel("#sovereign")
+s.ircchannels.append(c)
+
+os = OrderSet("home")
+order_1 = Order("t","u","info")
+order_2 = Order("t__2","u_2","info_2")
+os.orders.append(order_1)
+os.orders.append(order_2)
+
+s.ordersets.append(os)
+
+session.add(s)
+session.commit()
+
+bot = session.query(Sovereign).first()
+for chan in bot.ircchannels:
+	print chan.name + " " + chan.key
+
+
+os = bot.ordersets
+for orderset in bot.ordersets:
+    print orderset.name + " - " + orderset.orders.__repr__()
